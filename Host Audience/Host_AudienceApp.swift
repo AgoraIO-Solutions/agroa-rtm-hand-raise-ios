45147f236e79dcf91053433096be0ef7ebc9e19f
Shaun Hubbard <shaunhubbard2013@icloud.com>
2022-02-16 13:36:05 -0500

    Initial Commit

---
+++ Host Audience/Host_AudienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Host_AudienceApp.swift
+//  Host Audience
+//
+//  Created by shaun on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Host_AudienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
